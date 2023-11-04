0fd3eb63f45742d618cb311b68d201f678d597e4
Adrian <adrianqueizan@gmail.com>
2023-11-04 11:39:02 -0400

    saturday update

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,46 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import Signup from "./Singup";
 
-function Login(){
+function Login({setUser}){
 
-    const validationSchema = Yup.object({
-        username: Yup.string().required('Name is required'),
-        password: Yup.string().required('Password is required').
-        min(8, 'Password must be at least 8 characters long')
+  const validationSchema = Yup.object({
+    username: Yup.string().required('Name is required'),
+    password: Yup.string().required('Password is required').
+    min(8, 'Password must be at least 8 characters long')
+  })
+
+  const[signup,setsignup]=useState(false)
+
+  function handletoggle(e){
+    setsignup(!signup)
+  }
+
+  const handleSubmit = (values) => {
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: values.username,
+        password: values.password,
+      }),
+    })
+      .then((r) => {
+        if (r.ok) {
+          r.json().then((user) => setUser(user))
+        }
       })
+      .catch((error) => {
+        // Handle errors here
+        console.error("Error:", error)
+      })
+  }
+      
+    
+  const login = <div className="form">
+  <Formik
+    initialValues={{ username: '', password: '' }}
+    validationSchema={validationSchema}
+    onSubmit={handleSubmit}>
+    <Form>
+      <div>
+        <label htmlFor="username">User name:</label>
+        <Field type="text" id="username" name="username"/>
+        <ErrorMessage name="username" component="div" />
+      </div>
+
+      <div>
+        <label htmlFor="password">Password:</label>
+        <Field type="password" id="password" name="password"/>
+        <ErrorMessage name="password" component="div" />
+      </div>
+
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+    </Form>
+  </Formik>
+  </div>
 
     return (
-    <>
-    <Formik
-      initialValues={{ username: '', password: '' }}
-      validationSchema={validationSchema}
-      onSubmit={(values, actions) => {
-        // Handle form submission here, e.g., send data to the server
-        console.log(values);
-        actions.setSubmitting(false);
-      }}>
-        <Form>
-        <div>
-          <label htmlFor="username">User name:</label>
-          <Field type="text" id="username" name="username"/>
-          <ErrorMessage name="username" component="div" />
-        </div>
-
-        <div>
-          <label htmlFor="password">Password:</label>
-          <Field type="password" id="password" name="password"/>
-          <ErrorMessage name="password" component="div" />
-        </div>
-
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </Form>
-    </Formik>
-        
-    </>
+      <div className="login-signup">
+        {signup ? login : <Signup setUser={setUser}/>}
+        <button onClick={handletoggle}>{signup ? "Signup" : "Login"}</button>
+      </div>
+      
     )
+ 
 }
 
 export default Login 
--- client/src/components/UserGallery.js
+++ client/src/components/UserGallery.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
+import Cards from "./Cards";
 
 function UserGallery(){
-    return <div></div>
+    const [user, setUser] = useState(null);
+    const [cards,setCards]=useState([])
+    
+    useEffect(() => {
+        fetch("")
+        .then((res) => res.json())
+        .then((data) => setCards(data));
+      }, [])
+
+    return (
+        <>
+        {cards>0 ? <Cards cards={cards}/>: <h1>Loading...</h1>}
+        </>
+    )
 }
 
 export default UserGallery
